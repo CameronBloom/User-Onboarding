677c19051f16d68c5de67c4d24197553805a87f5
Cameron Morgan <cameron.morgan@skiff.com>
2023-03-06 14:56:16 -0800

    Completed the Advanced Form and implemented the Form Validation and Error Messaging.

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,11 +1,62 @@
-
 import './App.css';
 import Form from './components/Form';
+import { useState, useEffect } from 'react';
+import * as yup from 'yup';
+import axios from 'axios';
+
+const schema = yup.object().shape({
+  firstName: yup.string().required('first name is required'),
+  lastName: yup.string().required('last name is required'),
+  email: yup.string().email(),
+  password: yup.string().required('last name is required').min(8, 'password must be at least eight characters...'),
+  terms: yup.boolean().oneOf([true], "you MUST agree to the terms...")
+})
+
+const defaultData = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  terms: ""
+}
 
 function App() {
+  // state
+  const [form, setForm] = useState(defaultData);
+  const [disabled, setDisabled] = useState(true);
+  const [errors, setErrors] = useState(defaultData);
+
+  // use schema to identify errors
+  const setFormErrors = (name, value) => {
+    yup.reach(schema, name).validate(value)
+    // for an asynchronous promise, use then.()
+      .then(() => {
+        setErrors({ ...errors, [name]: '' })
+      })
+      .catch(err => {
+        setErrors({ ...errors, [name]: err.errors[0] })
+      })
+  }
+
+  // use onChange events to update the form or identify errors
+  const handleChange = event => {
+    const { checked, value, name, type } = event.target;
+    // if checkbox type, then use "checked" from event.target, else use "value" from event.target
+    const valueToUse = type === "checkbox" ? checked : value;
+    setFormErrors(name, valueToUse);
+    setForm({ ...form, [name]: valueToUse});
+  }
+
+  // use schema to validate button status - effect runs every time the "form" is updated
+  useEffect(() => {
+    schema.isValid(form)
+    .then(valid => setDisabled(!valid))
+    .catch(err => console.error(err))
+  }, [form])
+
   return (
     <div className="App">
-      <Form />
+      <Form disabled={disabled} setDisabled={setDisabled} handleChange={handleChange} form={form} errors={errors} />
     </div>
   );
 }
--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,8 +1,58 @@
 export default function Form(props) {
   return (
-    <div>
-      <h1>Test</h1>
-      <p>Paragraph</p>
-    </div>
+    <form>
+      <label>First Name&nbsp;
+        <input 
+          type="text"
+          id="firstName"
+          name="firstName"
+          value={props.form["firstName"]}
+          onChange={props.handleChange}
+        />
+      </label>
+      <div style={{ color: "red" }}>{props.errors.firstName}</div>
+      
+      <label>Last Name&nbsp;
+        <input 
+          type="text"
+          id="lastName"
+          name="lastName"
+          value={props.form["lastName"]}
+          onChange={props.handleChange}
+        />
+      </label>
+      <div style={{ color: "red" }}>{props.errors.lastName}</div>
+      <label>Email&nbsp;
+        <input 
+          type="email"
+          id="email"
+          name="email"
+          value={props.form["email"]}
+          onChange={props.handleChange}
+        />
+      </label>
+      <div style={{ color: "red" }}>{props.errors.email}</div>
+      <label>Password&nbsp;
+        <input 
+          type="password"
+          id="password"
+          name="password"
+          value={props.form["password"]}
+          onChange={props.handleChange}
+        />
+      </label>
+      <div style={{ color: "red" }}>{props.errors.password}</div>
+      <label>Terms of Service&nbsp;
+        <input 
+          type="checkbox"
+          id="terms"
+          name="terms"
+          checked={props.form["terms"]}
+          onChange={props.handleChange}
+        />
+      </label>
+      <div style={{ color: "red" }}>{props.errors.terms}</div>
+      <button disabled={props.disabled}>Submit</button>
+    </form>
   )
 }
